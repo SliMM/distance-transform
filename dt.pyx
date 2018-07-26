cd909d77e9d07865666364738ae549177e21d9a8
Ștefan Săftescu <stefan.saftescu@gmail.com>
2018-07-26 01:42:44 +0100

    Convert result of `map` to `list`.
    In Python 3, `map` returns a map object instead of a list. This means that the assignment on the next line fails with the message `TypeError: 'map' object does not support item assignment`.

--- dt.pyx
+++ dt.pyx
@@ -77,7 +77,7 @@ def compute(x, axes=None, f=L2):
     numel  = shape[axis]
     minbuf = np.empty((numel,), dtype=float)
     argbuf = np.empty((numel,), dtype=int)
-    slices = map(xrange, shape)
+    slices = list(map(xrange, shape))
     slices[axis] = [Ellipsis]
 
     for index in itertools.product(*slices):
